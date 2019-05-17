3a05200095881edc159822cacb74cabde7c0686d
happywe <15212931215@163.com>
2019-05-17 11:37:33 +0800

    the version of 4.1

--- pages/todos/todos.js
+++ pages/todos/todos.js
@@ -6,70 +6,97 @@ Page({
   data: {
     todos: [],
     inputVal: '',
+    keyTitle: '',
+    a: [],
     msgData: [
       { msg: '88888888888' }
     ]
   },
 
-  button: function () {
-    wx.navigateTo({
-      url: '/pages/search/search',
-    })
-  },
-
-//-----张家豪的爱心--------------------
-  onLoad: function (option) {
-    // 获取接收到的id值
-    var getId = option.id;
-    // 让接收到的id值传递到data:{}里面
+  //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  //获取用户输入的关键字
+  keyInput: function (e) {
+    // console.log("用户名：" + e.detail.value)
     this.setData({
-      currentId: getId
+      userName: e.detail.value
+    })
+    //将匹配到的选项放到b中
+    var b = []
+    var count = 0
+    var flag = 0
+    var trans = e.currentTarget.dataset.trans
+    for (var j = 0; j < e.detail.value.length; j++) {
+      for (var i = 0; i < trans.length; i++) {
+        console.log(trans[0].title.substring(0, 1))
+        if (e.detail.value == trans[i].title.substring(0, j + 1)) {
+          b[count] = trans[i]
+          count++
+        }
+      }
+    }
+
+    for (var n = 0; n < b.length; n++) {
+      console.log("+++++++++++++++++++" + b[n].title)
+    }
+
+    // for (var n=0;n<b.length-1;n++){
+    //   for (var j = 0; j < b.length ; j++)
+    //   if(b[n].title == b[j].title){
+    //     b.splice(j,1)
+    //   }
+    // }  
+    this.setData({
+      a: b
     });
-    // 读取所有的文章列表点赞缓存状态
-    var cache = wx.getStorageSync('cache_key');
-    // 如果缓存状态存在
-    if (cache) {
-      // 拿到所有缓存状态中的1个
-      var currentCache = cache[getId];
-      // 把拿到的缓存状态中的1个赋值给data中的collection，如果当前文章没有缓存状态，currentCache 的值就是 false，如果当前文章的缓存存在，那么 currentCache 就是有值的，有值的说明 currentCache 的值是 true
-      this.setData({
-        collection: currentCache
-      })
-    } else {
-      // 如果所有的缓存状态都不存在 就让不存在的缓存存在
-      var cache = {};
-      // 既然所有的缓存都不存在，那么当前这个文章点赞的缓存也不存在，我们可以把当前这个文章点赞的缓存值设置为 false
-      cache[getId] = false;
-      // 把设置的当前文章点赞放在整体的缓存中
-      wx.setStorageSync('cache_key', cache);
+
+    // console.log("akakakakakkkakak"+a.title)
+  },
+  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+  //获取用户输入的密码
+  BtnClick: function (e) {
+    console.log("用户名：" + this.data.userName);
+    // userNameInput()
+  },
+  //+++++++++++++++++++++++++++++++++++++++++++++
+  button: function (e) {
+    var trans = e.currentTarget.dataset.trans
+    wx.navigateTo({
+      url: '/pages/search/search?skip=' + JSON.stringify(trans)
+    })
+    for (var n = 0; n < trans.length; n++) {
+      console.log("第一个界面的值" + trans[n].title)
     }
   },
 
-  toCollect: function (event) {
-    // 获取所有的缓存
-    var cache = wx.getStorageSync('cache_key');
-    // 获取当前文章是否被点赞的缓存
-    var currentCache = cache[this.data.currentId];
-    // 取反，点赞的变成未点赞 未点赞的变成点赞
-    currentCache = !currentCache;
-    // 更新cache中的对应的1个的缓存值，使其等于当前取反的缓存值
-    cache[this.data.currentId] = currentCache;
-    // 重新设置缓存
-    wx.setStorageSync('cache_key', cache);
-    // 更新数据绑定,从而切换图片
+
+//-----张家豪的爱心--------------------
+  
+  setlove: function (e) {
+    var objectId = e.currentTarget.dataset.id;
+    var flag = e.currentTarget.dataset.flag;
+
+    flag = !flag;
+    console.log(flag);
+    const user = AV.User.current()
+    console.log(user.id)
+    console.log(e.currentTarget.dataset.index);
+    var todo = AV.Object.createWithoutData('Todo', objectId);
+    todo.set('flag', flag);
+    todo.save().then(function (todo) {
+      console.log("ok!")
+    });
+    console.log("表示:" + objectId + "flag:" + flag)
+
     this.setData({
-      // collection 默认的是 false
-      collection: currentCache
+      f: flag
     });
-    // 交互反馈
-    wx.showToast({
-      title: currentCache ? '点赞' : '取消',
-      icon: 'success',
-      duration: 2000
-    });
+
+    this.onLoad()
+    this.onShow()
   },
 
-//--------------------张家豪的爱心-----------------------------------------
+
 
   login: function() {
     return AV.Promise.resolve(AV.User.current())
